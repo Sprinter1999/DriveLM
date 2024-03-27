49725ac764fd440b4b7e4c62754347df2efaebe1
Xuefen <215437588@qq.com>
2024-03-27 16:54:57 +0800

    update convert codes

--- challenge/convert_nuscenesMQA.py
+++ challenge/convert_nuscenesMQA.py
@@ -2,63 +2,67 @@ import csv
 import json
 from nuscenes.nuscenes import NuScenes
 import re
-
-
-nusc = NuScenes(version='v1.0-trainval', dataroot='./data/nuscenes', verbose=True) 
-# 创建一个映射以跟踪已经分配的ID  
-id_map = {} 
  
-
+ 
+nusc = NuScenes(version='v1.0-trainval', dataroot='./data/nuscenes', verbose=True)
+# 创建一个映射以跟踪已经分配的ID 
+id_map = {}
+  
+ 
 # These two functions should be implemented according to your dataset access patterns
 def get_image_paths_and_scene(sample_token):
     # This function should return a list of image paths for the given sample_token
     # Replace the following example paths with the actual logic to obtain relevant paths
     sample = nusc.get('sample', sample_token)
-    front_image_path = nusc.get('sample_data', sample['data']['CAM_FRONT'])['filename'] 
-    fl_image_path = nusc.get('sample_data', sample['data']['CAM_FRONT_LEFT'])['filename']
-    fr_image_path = nusc.get('sample_data', sample['data']['CAM_FRONT_RIGHT'])['filename']
-    back_image_path = nusc.get('sample_data', sample['data']['CAM_BACK'])['filename']
-    bl_image_path = nusc.get('sample_data', sample['data']['CAM_BACK_LEFT'])['filename']
-    br_image_path = nusc.get('sample_data', sample['data']['CAM_BACK_RIGHT'])['filename']
+    prefix = "data/nuscenes/"
+     
+    front_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_FRONT'])['filename']
+    fl_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_FRONT_LEFT'])['filename']
+    fr_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_FRONT_RIGHT'])['filename']
+    back_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_BACK'])['filename']
+    bl_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_BACK_LEFT'])['filename']
+    br_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_BACK_RIGHT'])['filename']
     scene_token = sample['scene_token']
-
-
+ 
+ 
     return   front_image_path,fl_image_path,fr_image_path,back_image_path,bl_image_path,br_image_path,scene_token
-    
-
-
+     
+ 
+ 
 def convert_csv_to_json(csv_filepath):
     data = []  # List to store the converted data
-    
+     
     # Open the CSV file and read each row
     with open(csv_filepath, newline='', encoding='utf-8') as csvfile:
         csvreader = csv.reader(csvfile)
         print("### Original file loaded")
         next(csvreader, None)  # Skip the header row if there is one
         for row in csvreader:
-            line_num, sample_token, question, answer, _ = row  # Assuming four columns; ignore question_type
+            #print(row)
+            # train has 5 cols (line_num, sample_token...) while val has 4 cols (sample_token,...)
+            line, sample_token, question, answer, _ = row  # Assuming four columns; ignore question_type
             front_image_path,fl_image_path,fr_image_path,back_image_path,bl_image_path,br_image_path,scene_token = get_image_paths_and_scene(sample_token)
-
+ 
             sample_id=None
-            if sample_token in id_map:  
+            if sample_token in id_map: 
                 id_map[sample_token] += 1
                 sample_id = scene_token + "_" + sample_token + "_" + str(id_map[sample_token])
-            else:  
-                # id = len(id_map) + 1  
-                id_map[sample_token] = 1  
+            else: 
+                # id = len(id_map) + 1 
+                id_map[sample_token] = 1 
                 sample_id = scene_token + "_" + sample_token + "_1"
-
-
+ 
+ 
             filtered_question = re.sub(r'<[^>]*>', '', question)
             filtered_answer = re.sub(r'<[^>]*>', '', answer)
-
+ 
             """
             Example:
             Source:  In the <cam>front left</cam>, <target><cnt>2</cnt> <obj>bikes</obj></target> are detected.
             Convert: In the front left, 2 bikes are detected.
             """
-
-
+ 
+ 
             # entry = {
             #     "id": sample_id,
             #     "image": image_paths,
@@ -67,27 +71,27 @@ def convert_csv_to_json(csv_filepath):
             #         {"from": "gpt", "value": "<image>\n" + filtered_answer}
             #     ]
             # }
-            entry={        
-            "id": sample_id,        
-            "image": [front_image_path,fl_image_path,fr_image_path,back_image_path,bl_image_path,br_image_path       
-            ],      
+            entry={       
+            "id": sample_id,       
+            "image": [front_image_path,fl_image_path,fr_image_path,back_image_path,bl_image_path,br_image_path      
+            ],     
                 "conversations": [
                     {"from": "human", "value": filtered_question},
                     {"from": "gpt", "value": "<image>\n" + filtered_answer}
-                ] 
+                ]
             }
-            
+             
             data.append(entry)
-    
+     
     # Write the JSON output
     # with open(json_filepath, 'w', encoding='utf-8') as jsonfile:
     #     json.dump(data, jsonfile, ensure_ascii=False, indent=4)
-    with open(output_file_path, "w", encoding="utf-8") as output_file:  
-        json.dump(data, output_file, indent=4)   
-
+    with open(output_file_path, "w", encoding="utf-8") as output_file: 
+        json.dump(data, output_file, indent=4)  
+ 
 # Example usage
 # csv_file_path = 'val.csv'
-# "./nuScenes-MQA/df_train_mqa.json"  and "./nuScenes-MQA/df_val_mqa.json" 
-file_path = "./nuScenes-MQA/df_train_mqa.json" 
-output_file_path = "processed_nuscenesMQA_train.json" 
+# "./nuScenes-MQA/df_train_mqa.csv"  and "./nuScenes-MQA/df_val_mqa.csv"
+file_path = "./nuScenes-MQA/df_train_mqa.csv"
+output_file_path = "processed_nuscenesMQA_train_V2.json"
 convert_csv_to_json(file_path)
--- challenge/convert_nuscenesQA.py
+++ challenge/convert_nuscenesQA.py
@@ -1,75 +1,76 @@
-import json  
-import os  
+import json 
+import os 
 from nuscenes.nuscenes import NuScenes
-
+ 
 # need to modify
-nusc = NuScenes(version='v1.0-trainval', dataroot='./data/nuscenes', verbose=True) 
-
-
+nusc = NuScenes(version='v1.0-trainval', dataroot='./data/nuscenes', verbose=True)
+ 
+ 
 # 读取原始JSON文件, val比较小，先尝试val, then train
-file_path = "./nuScenes-QA/NuScenes_train_questions.json"  
-with open(file_path, "r", encoding="utf-8") as file:  
-    data = json.load(file) 
+file_path = "./nuScenes-QA/NuScenes_val_questions.json" 
+with open(file_path, "r", encoding="utf-8") as file: 
+    data = json.load(file)
     print("Loaded Original Json file...")
-  
-# 创建一个映射以跟踪已经分配的ID  
-id_map = {}  
-processed_data = []  
-  
-# 处理数据  
-for item in data["questions"]:  
-    sample_token = item["sample_token"]  
-    question = "<image>\n" + item["question"]  
-    answer =  item["answer"]  # 在answer前添加前缀"<image>\n" 
+   
+# 创建一个映射以跟踪已经分配的ID 
+id_map = {} 
+processed_data = [] 
+   
+# 处理数据 
+for item in data["questions"]: 
+    sample_token = item["sample_token"] 
+    question = "<image>\n" + item["question"] 
+    answer =  item["answer"]  # 在answer前添加前缀"<image>\n"
     sample_id = None
     #print(f"Sample Token {sample_token}")
-
+ 
     # get samo
-    sample = nusc.get('sample', sample_token)  
-
+    sample = nusc.get('sample', sample_token) 
+ 
     scene_token = sample['scene_token']
-    # image_path_front = sample['data']['CAM_FRONT']  
-    front_image_path = nusc.get('sample_data', sample['data']['CAM_FRONT'])['filename'] 
-    fl_image_path = nusc.get('sample_data', sample['data']['CAM_FRONT_LEFT'])['filename']
-    fr_image_path = nusc.get('sample_data', sample['data']['CAM_FRONT_RIGHT'])['filename']
-    back_image_path = nusc.get('sample_data', sample['data']['CAM_BACK'])['filename']
-    bl_image_path = nusc.get('sample_data', sample['data']['CAM_BACK_LEFT'])['filename']
-    br_image_path = nusc.get('sample_data', sample['data']['CAM_BACK_RIGHT'])['filename']
-
-    
+    # image_path_front = sample['data']['CAM_FRONT']
+    prefix = "data/nuscenes/"
+    front_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_FRONT'])['filename']
+    fl_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_FRONT_LEFT'])['filename']
+    fr_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_FRONT_RIGHT'])['filename']
+    back_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_BACK'])['filename']
+    bl_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_BACK_LEFT'])['filename']
+    br_image_path = prefix + nusc.get('sample_data', sample['data']['CAM_BACK_RIGHT'])['filename']
+ 
+     
     times = None
     # 如果已经为该sample_token分配了ID，则重用该ID，否则创建一个新的ID, 整体的id结构为 scenetoken_sample_token_X
-    if sample_token in id_map:  
+    if sample_token in id_map: 
         id_map[sample_token] += 1
         sample_id = scene_token +"_"+ sample_token + "_" + str(id_map[sample_token])
-    else:  
-        # id = len(id_map) + 1  
-        id_map[sample_token] = 1  
+    else: 
+        # id = len(id_map) + 1 
+        id_map[sample_token] = 1 
         sample_id = scene_token +"_"+sample_token + "_1"
-    
+     
     #print(f"front_image_path: {front_image_path}")
     # 添加处理后的数据
-    processed_data.append({        
-    "id": sample_id,        
-    "image": [front_image_path,  fl_image_path,   fr_image_path,  back_image_path, bl_image_path,   br_image_path        
-    ],      
-    "conversations": [      
-        {    
-            "from": "human",    
-            "value": question    
-        },    
-        {    
-            "from": "gpt",    
-            "value": answer    
-        }    
-    ]      
-    })     
-  
-# 输出处理后的JSON数据  
+    processed_data.append({       
+    "id": sample_id,       
+    "image": [front_image_path,  fl_image_path,   fr_image_path,  back_image_path, bl_image_path,   br_image_path       
+    ],     
+    "conversations": [     
+        {   
+            "from": "human",   
+            "value": question   
+        },   
+        {   
+            "from": "gpt",   
+            "value": answer   
+        }   
+    ]     
+    })    
+   
+# 输出处理后的JSON数据 
 #print(processed_data)
-#print(json.dumps(processed_data, indent=4)) 
-
+#print(json.dumps(processed_data, indent=4))
+ 
 # 输出处理后的JSON数据到文件,train and val
-output_file_path = "processed_nuscenesqa_train.json"  
-with open(output_file_path, "w", encoding="utf-8") as output_file:  
-    json.dump(processed_data, output_file, indent=4)   
+output_file_path = "processed_nuscenesqa_val_V2.json" 
+with open(output_file_path, "w", encoding="utf-8") as output_file: 
+    json.dump(processed_data, output_file, indent=4)  
